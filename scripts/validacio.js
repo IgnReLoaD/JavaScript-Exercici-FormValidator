9550c5619be7db37cc961576e96668df8aeea051
IgnReLoaD <ortiz.ignasi@gmail.com>
2021-03-15 19:25:09 +0100

    validacio Pwd Mail Telf

--- scripts/validacio.js
+++ scripts/validacio.js
@@ -9,7 +9,7 @@ var passwor2 = document.forms['frmReg']['inpPwd2'];
 var realname = document.forms['frmReg']['inpName'];
 var surnames = document.forms['frmReg']['inpSurn'];
 var email    = document.forms['frmReg']['inpMail'];
-// var telefono = document.forms['frmReg']['inpTelf'];
+var telefono = document.forms['frmReg']['inpTelf'];
 var direccio = document.forms['frmReg']['inpDire'];
 var poblacio = document.forms['frmReg']['inpPobl'];
 var province = document.forms['frmReg']['inpProv'];
@@ -28,28 +28,30 @@ var email_error     = document.getElementById('errMail');
 var telefono_error  = document.getElementById('errTelf');
 var password_error  = document.getElementById('errPwd1');
 var passwor2_error  = document.getElementById('errPwd2');
+var missatge_envio  = document.getElementById('msgForm');
+var btn_reset_form  = document.getElementById('btnRset');
+var btn_regis_form  = document.getElementById('btnRegi');
+
 var finestra_modal  = document.getElementById('miModal');
 var form_registrar  = document.getElementById('frmReg');
 
 // DEFINIR EVENTS - quan el camp perd focus, si ha canviat dispara funció corresponent
 username.addEventListener('focusout', userVerify);
-realname.addEventListener('change', realVerify);
-email.addEventListener('change', mailVerify);
-password.addEventListener('change', pwd1Verify);
-passwor2.addEventListener('change', pwd2Verify);
-// telefono.addEventListener('change', telfVerify);
+realname.addEventListener('focusout', realVerify);
+email.addEventListener('focusout', mailVerify);
+password.addEventListener('focusout', pwd1Verify);
+passwor2.addEventListener('focusout', pwd2Verify);
+telefono.addEventListener('change', telfVerify);
 
 
-// al iniciar la finestra, posem el focus sobre el primer camp -- NO FUNCIONA ------
+// -------- FOCUS AL PRIMER CAMP al iniciar la finestra -----------------------------
 // finestra_modal.onload = function(){
 //   username.focus();
 // }
-
 // aixi tampoc
 // function focusPrimerCamp(){
 //   username.focus();
 // }
-
 // aixi tampoc funciona
 // finestra_modal.addEventListener('onload', focusPrimerCamp);
 
@@ -60,7 +62,6 @@ $(function () {
 });
 
 // ------------ FINS AQUI EXECUTA AL CRIDAR EL JS -----------------------------------
-
 
 // DEFINIR FUNCIO INVALIDAR: afegeix la classe 'is-invalid' al camp erroni, que fa:
 //                         1. mostra label en vermell i símbol admiració vermell
@@ -78,26 +79,25 @@ function Validar(camp) {
   camp.classList.remove("is-invalid");
 }
 
+// DEIXAR ELS INPUTS I LABELS INTACTES DE NOU (a nivell de bootstrap)
 function SenseValidar(camp){
   camp.classList.remove("is-valid");
   camp.classList.remove("is-invalid");
   camp.style.border = "1px solid black";  
 }
 
+// BUIDAR EL CONTINGUT DELS INPUTS (es cridat pel botó RESET que els esborra, i per X)
 function buidar(){
   document.getElementById("frmReg").reset();
-
   let arrDivs = document.getElementsByClassName("frmDivs");
-  // arrDivs[0].style.color = "black";
-  // arrDivs[1].style.color = "black";
-  // arrDivs.map( (key) => {
-  //     key.style.color = "black" });
-
+      // arrDivs[0].style.color = "black";
+      // arrDivs[1].style.color = "black";
+      // arrDivs.map( (key) => {
+      //     key.style.color = "black" });
   // un bucle que recorri aquest array.
   for (item of arrDivs){
     item.style.color = "black";
   }
-
   SenseValidar(username);
   SenseValidar(password);
   SenseValidar(passwor2);
@@ -107,19 +107,6 @@ function buidar(){
 
   // username.textContent="";
   username.focus();
-}
-
-// DEFINIR FUNCIO PRINCIPAL (main) - quan pulsa ENVIAR ho repassa tot
-function Validate() {
-    let validador = true;
-    form.classList.remove("is-invalid");
-
-    if ( userVerify() && pwd1Verify() && realVerify() && mailVerify() && telfVerify() ) {
-      validador = true;
-    }else{
-      validador = false;
-    }
-    return validador;
 }
 
 // VERIFICAR el camp USER - quan perd focus, si cambia contingut inputBox, avisa en Vermell
@@ -164,6 +151,29 @@ function userVerify() {
     return boleano;
 }
 
+// VERIFICAR el camp Pwd1 - quan perd focus, si cambia contingut inputBox, avisa en Vermell
+function pwd1Verify(){
+    let boleano = false;
+
+    if (inpPwd1.value == ""){
+      Invalidar(inpPwd1);
+      password_error.textContent = "No es pot deixar en blanc."
+    } else if (inpPwd1.value.length < 8) {
+      Invalidar(inpPwd1);
+      password_error.textContent = "Mínim 8 caracters (maj, min, num, simbol)."
+    } else if ((Cumpleix(inpPwd1.value))== false) {
+      Invalidar(inpPwd1);
+      password_error.textContent = "Contenir mínim 1 majús 1 minús 1 num 1 simbol."
+    } else {
+      password.style.border = "1px solid #5e6e66";
+      document.getElementById('divPwd1').style.color = "#0ec771";
+      password_error.textContent = "";
+      boleano = true;
+      Validar(password);
+    }
+    return boleano;
+}
+
 function Cumpleix(cadena){
   let bolMayus  = false;
   const mayusc  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -190,43 +200,135 @@ function Cumpleix(cadena){
   return (bolMayus && bolMinus && bolSimbol && bolNumero);
 }
 
-// VERIFICAR el camp Pwd1 - quan perd focus, si cambia contingut inputBox, avisa en Vermell
-function pwd1Verify(){
-    let booleano = false;
-
-    if (inpPwd1.value == ""){
-      Invalidar(inpPwd1);
-      password_error.textContent = "No es pot deixar en blanc."
-    } else if (inpPwd1.value.length < 8) {
-      Invalidar(inpPwd1);
-      password_error.textContent = "Mínim 8 caracters (maj, min, num, simbol)."
-    } else if ((Cumpleix(inpPwd1.value))== false) {
-      Invalidar(inpPwd1);
-      password_error.textContent = "Contenir mínim 1 majús 1 minús 1 num 1 simbol."
-    } else {
-      password.style.border = "1px solid #5e6e66";
-      document.getElementById('divPwd1').style.color = "#0ec771";
-      password_error.textContent = "";
-      boleano = true;
-      Validar(password);
-    }
-    return booleano;
-}
-
 function pwd2Verify(){
+    let boleano = false;
 
+    if(inpPwd2.value == "") {
+      Invalidar(inpPwd2);
+      password_error.textContent = "No es pot deixar en blanc.";
+    } else if (inpPwd2.value !== inpPwd1.value) {
+      Invalidar(inpPwd2);
+      passwor2_error.textContent = "No coincideixen les pwd.";
+    } else  {
+      passwor2.style.border = "1px solid #5e6e66";
+      document.getElementById('divPwd2').style.color = "#0ec771";
+      passwor2_error.textContent = "";
+      boleano = true;
+      Validar(passwor2);
+    }
+    return boleano;    
 }
+
+// VERIFICAR el camp Nom - quan perd focus, si cambia contingut inputBox, avisa en Vermell
 
 function realVerify(){
-
+  let boleano = false;
+  // si tot és correcte, simplement posa en verd un detall 
+  if ((realname.value.length >= 3) && (realname.value != "")) {
+    realname.style.border = "1px solid #5e6e66";
+    document.getElementById('divName').style.color = "#0ec771";
+    realname_error.textContent = "";    
+    boleano = true;
+    Validar(realname);
+  } else {
+    realname.style.border = "1px solid red";
+    document.getElementById('divName').style.color = "red";
+    realname_error.textContent = "Entri un nom correcte.";
+    realname.focus();
+    validador = false; 
+    Invalidar(realname);
+  }
+  return boleano;
 }
+
+// VERIFICAR el camp TELEFON - quan perd focus, si cambia contingut inputBox
 
 function telfVerify(){
-  
+  let boleano = false;
+  // si esta en blanc
+  if (inpTelf.value == "") {
+    Invalidar(inpTelf);
+    telf_error.textContent = "No es pot deixar en blanc.";
+  } else if (!cumpleixTelf(inpTelf.value)) {
+    Invalidar(inpTelf);
+    telf_error.textContent = "No cumpleix format standard.";
+  } else {
+    telefono.style.border = "1px solid #5e6e66";
+    document.getElementById('divTelf').style.color = "#0ec771";
+    telf_error.textContent = "";    
+    boleano = true;
+    Validar(inpTelf);
+  }
+  return boleano;
 }
 
+// Funcio recursiva (reutilitzable) a part, per validar mail 
+function cumpleixTelf(telf) {
+	var regex = /^([0-9-])+\-([0-9-])+\-([0-9-])+$/;
+	return regex.test(telf) ? true : false;
+}
+
+
+// VERIFICAR el camp EMAIL - quan perd focus, si cambia contingut inputBox
+
 function mailVerify(){
-  
+  let boleano = false;
+  // si esta en blanc
+  if (inpMail.value == "") {
+    Invalidar(inpMail);
+    email_error.textContent = "No es pot deixar en blanc.";
+  } else if (!cumpleixEmail(inpMail.value)) {
+    Invalidar(inpMail);
+    email_error.textContent = "No cumpleix format standard.";
+  } else {
+    email.style.border = "1px solid #5e6e66";
+    document.getElementById('divMail').style.color = "#0ec771";
+    email_error.textContent = "";    
+    boleano = true;
+    Validar(email);
+  }
+  return boleano;
+}
+
+// Funcio recursiva (reutilitzable) a part, per validar mail 
+function cumpleixEmail(email) {
+	var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	return regex.test(email) ? true : false;
 }
 
 username.focus();
+
+// ============ FUNCIO PRINCIPAL (main) - quan pulsa ENVIAR ho repassa tot =============
+function validate() {
+  let validador = true;
+  const form = document.getElementById('frmReg');
+  form.classList.remove("is-invalid");
+
+  // if ( userVerify() && pwd1Verify() && pw2Verify() && realVerify() && mailVerify() && telfVerify() ) {
+  if ( userVerify() ) {
+    validador = true;
+    msgForm.innerHTML = "Enviat correctament.";
+    msgForm.style.color = "green";
+    // btn_regis_form.style.disabled = true;
+    // sleep(5000);
+  }else{
+    validador = false;
+  }
+  return validador;
+}
+
+// function sleep(milisegons) {
+//   var start = new Date().getTime();
+//   for (var i = 0; i < 1e7; i++) {
+//       msgForm.innerHTML = "Enviat correctament.";
+//       msgForm.style.color = "green";
+//       btn_regis_form.style.color = "gray";
+//       btn_regis_form.style.disabled = true;
+//       btn_reset_form.style.color = "gray";    
+
+//       if ((new Date().getTime() - start) > milisegons) {
+//           break;
+//       }
+//   }
+//  }
+
